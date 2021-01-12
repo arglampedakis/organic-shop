e511fe2647a10d930279e9a13bf0aa9646908ef2
Argiris Gl <glambedos18@hotmail.com>
2021-01-12 17:28:23 +0200

    Added all the components we'll need. Adjusted  Navbar component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAY6px1V9XYjfGAIXKupAq3pjAzTFC8tzQ",
-    authDomain: "oshop-fc783.firebaseapp.com",
-    databaseURL: "https://oshop-fc783-default-rtdb.firebaseio.com",
-    projectId: "oshop-fc783",
-    storageBucket: "oshop-fc783.appspot.com",
-    messagingSenderId: "1056869147783",
-    appId: "1:1056869147783:web:98caf1d306c27e8d1b0422",
-    measurementId: "G-C5N0YFWLKS"
+    apiKey: "AIzaSyDG1CnzbSDtQe0Jp2VWJAd3xCYUj4XdsnI",
+    authDomain: "oshop-326ae.firebaseapp.com",
+    databaseURL: "https://oshop-326ae-default-rtdb.firebaseio.com",
+    projectId: "oshop-326ae",
+    storageBucket: "oshop-326ae.appspot.com",
+    messagingSenderId: "340023342623",
+    appId: "1:340023342623:web:c41c9ee7ddbd1e4b8a072a",
+    measurementId: "G-LT9GTEXJ57"
   }
 };
