1e7349885cd939ae51ebedfb75791a532a40cf16
Argiris Gl <glambedos18@hotmail.com>
2021-01-12 12:58:44 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAY6px1V9XYjfGAIXKupAq3pjAzTFC8tzQ",
+    authDomain: "oshop-fc783.firebaseapp.com",
+    databaseURL: "https://oshop-fc783-default-rtdb.firebaseio.com",
+    projectId: "oshop-fc783",
+    storageBucket: "oshop-fc783.appspot.com",
+    messagingSenderId: "1056869147783",
+    appId: "1:1056869147783:web:98caf1d306c27e8d1b0422",
+    measurementId: "G-C5N0YFWLKS"
+  }
 };
 
 /*
