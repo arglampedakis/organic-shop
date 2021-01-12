4b6f9f531e71204838e6a4eb9de4c65610f34f8a
a_glampedakis <61798869+arglampedakis@users.noreply.github.com>
2021-01-12 21:24:13 +0200

    implemented login feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  ngOnInit(): void {
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.default.auth.GoogleAuthProvider());
   }
-
 }
