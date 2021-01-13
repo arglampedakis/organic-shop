a960ebc2454fe17774d4455747b583c68dd109f0
Argiris Gl <glambedos18@hotmail.com>
2021-01-13 15:20:29 +0200

    implemented returnUrl feature to navigate users after login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'oshop';
+  constructor(private auth: AuthService, router: Router){
+    //Since this is the App component *root component of our application), 
+    //there's no need to unsubscribe from this subscription
+    auth.user$.subscribe(
+      user => {
+        if(user){
+          let returnUrl = localStorage.getItem('returnUrl');
+          router.navigateByUrl(returnUrl);
+        }
+      }
+    );
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,14 @@ export class AuthService {
 
   user$: Observable<firebase.default.User>;
 
-  constructor(private afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private router: Router) { 
     this.user$ = afAuth.authState;
   }
 
   login(){
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
     this.afAuth.signInWithRedirect(new firebase.default.auth.GoogleAuthProvider());
   }
 
