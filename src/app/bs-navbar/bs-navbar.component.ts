7b13a73663806d6864e02028ae92a19db08848b0
Argiris Gl <glambedos18@hotmail.com>
2021-01-13 14:20:34 +0200

    added logout feature

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  user$: Observable<firebase.default.User>;
 
-  ngOnInit(): void {
+  constructor(private afAuth: AngularFireAuth) { 
+    this.user$ = afAuth.authState;
   }
 
+  logout(){
+    this.afAuth.signOut();
+  }
 }
